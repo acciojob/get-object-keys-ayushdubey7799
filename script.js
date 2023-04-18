e4fe24b06d10bfaf090f4f18dfe7ff9e8d7af308
ayushdubey7799 <113593098+ayushdubey7799@users.noreply.github.com>
2023-04-18 14:53:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const student = {
+	name: 'xyz'
+};
+function studentInfo() {
+	return Object.keys(student);
+}
+student.__proto__.getKeys = studentInfo;
+studentInfo();
